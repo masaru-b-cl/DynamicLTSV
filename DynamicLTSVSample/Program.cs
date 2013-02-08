b403e7c885d0be5fac06e7e27d7b38a05de2b71c
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-02-08 14:41:11 +0900

    add sample project

---
+++ DynamicLTSVSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLTSVSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // parse line
+      var line = DynamicLTSV.ParseLine("hoge:foo\tbar:baz\n");
+      Console.WriteLine(line.hoge); // foo
+      Console.WriteLine(line.bar);  // baz
+
+      // oarse multi lines
+      var lines = DynamicLTSV.Parse(@"hoge:foo
+bar:baz
+");
+      Console.WriteLine(lines.First().hoge);  // foo
+      Console.WriteLine(lines.Last().bar);  // baz
+    }
+  }
+}
