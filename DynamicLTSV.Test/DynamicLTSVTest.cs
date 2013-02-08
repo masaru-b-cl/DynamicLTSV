31e1b82be29dbe385ca52d6ce5bfd06334e3eb88
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-02-08 13:15:07 +0900

    initial commit

---
+++ DynamicLTSV.Test/DynamicLTSVTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicLTSV.Test
+{
+  [TestClass]
+  public class DynamicLTSVTest
+  {
+    [TestMethod]
+    public void TestParseLine()
+    {
+      dynamic result = DynamicLTSV.ParseLine("label:text");
+      string label = result.label;
+      label.Is("text");
+    }
+  }
+}
---
+++ DynamicLTSV/DynamicLTSV.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLTSV
+{
+  public class DynamicLTSV : DynamicObject
+  {
+    private NameValueCollection source;
+
+    public DynamicLTSV()
+    {
+      this.source = new NameValueCollection();
+    }
+
+    public DynamicLTSV(string line)
+      : this()
+    {
+      this.source.Add("label", "text");
+    }
+
+    public static dynamic ParseLine(string line)
+    {
+      return new DynamicLTSV(line);
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    {
+      var value = source[binder.Name];
+      result = new StringMember(value);
+      return true;
+    }
+
+    public override bool TryConvert(ConvertBinder binder, out object result)
+    {
+      if (binder.Type != typeof(string))
+      {
+        result = null;
+        return false;
+      }
+      else
+      {
+        result = this.ToString();
+        return true;
+      }
+    }
+  }
+
+  class StringMember : DynamicObject
+  {
+    readonly string value;
+
+    public StringMember(string value)
+    {
+      this.value = value;
+    }
+
+    public override bool TryInvoke(InvokeBinder binder, object[] args, out object result)
+    {
+      var defaultValue = args.First();
+
+      try
+      {
+        result = (value == null)
+            ? defaultValue
+            : Convert.ChangeType(value, defaultValue.GetType());
+      }
+      catch (FormatException)
+      {
+        result = defaultValue;
+      }
+
+      return true;
+    }
+
+    public override bool TryConvert(ConvertBinder binder, out object result)
+    {
+      try
+      {
+        var type = (binder.Type.IsGenericType && binder.Type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            ? binder.Type.GetGenericArguments().First()
+            : binder.Type;
+
+        result = (value == null)
+            ? null
+            : type == typeof(DateTime)
+              ? DateTime.ParseExact(value, new[] { "yyyyMMdd", "yyyy/MM/dd", "yyMMdd", "yy/MM/dd" }, null, System.Globalization.DateTimeStyles.None)
+              : Convert.ChangeType(value, binder.Type);
+      }
+      catch (FormatException)
+      {
+        result = null;
+      }
+
+      return true;
+    }
+
+    public override string ToString()
+    {
+      return value ?? "";
+    }
+  }
+
+}
